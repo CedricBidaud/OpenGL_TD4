aeccf4a7c003a68fda7fe014fc23b9fdd9fb45fb
CedricBidaud <cb1989@hotmail.fr>
2012-10-30 08:28:12 +0100

    commit dossier ex1

---
+++ ex1/shaders/transform.vs.glsl
@@ -0,0 +1,14 @@
+#version 330
+
+layout(location = 0) in vec3 inVertexPosition;
+layout(location = 1) in vec3 inVertexNormal;
+layout(location = 2) in vec2 inVertexTexture;
+
+
+out vec3 vNormal;
+out vec2 vTexCoords;
+
+
+void main(){
+
+}
